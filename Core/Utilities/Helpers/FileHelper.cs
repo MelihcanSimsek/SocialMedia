219f32a03226758ebbc3aa1987b8b27d5f9b924a
Melih Şimşek <Melihsimsek02@gmail.com>
2023-11-02 21:11:13 +0300

    adding new utilities

---
+++ Core/Utilities/Helpers/FileHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Helpers
+{
+    public static class FileHelper
+    {
+        public static string Add(IFormFile file)
+        {
+            return "";
+        }
+    }
+}
